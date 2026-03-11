9dd55c5c67337566da3d2735ae125a4494168a5c
Guppy <guppy@hnsn.io>
2026-03-11 07:25:34 -0500

    test(cmd): cover run path helper in main test

--- cmd/sq/main_test.go
+++ cmd/sq/main_test.go
@@ -1,9 +1,21 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestRunHelp(t *testing.T) {
 	if code := run([]string{"help"}); code != 0 {
 		t.Fatalf("expected help code 0 got %d", code)
+	}
+}
+
+func TestMainSetsExitCodeViaRunPath(t *testing.T) {
+	oldArgs := os.Args
+	defer func() { os.Args = oldArgs }()
+	os.Args = []string{"sq", "help"}
+	if code := run(os.Args[1:]); code != 0 {
+		t.Fatalf("expected code 0 got %d", code)
 	}
 }
